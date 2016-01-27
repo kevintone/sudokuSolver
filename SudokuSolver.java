7c846ea0c01bac8209d2575f69ca81335f1cea72
kevintone <kevintone@hawaii.edu>
2016-01-26 23:14:27 -1000

    Added checkRow and checkColumn

--- SudokuSolver.java
+++ SudokuSolver.java
@@ -17,6 +17,11 @@ public class SudokuSolver {
 		//Initialize the board
 		boardArray = new int[9][9];
 		scan = new Scanner(System.in);
+		
+		//Get the user input
+		getBoard();
+		//Print the user inputted board
+		printBoard();
 	}
 	
 	public void getBoard() {
@@ -26,7 +31,6 @@ public class SudokuSolver {
 
 		for (int i = 0; i < 9; i++) {
 			String input = scan.nextLine();
-			System.out.println(input);
 			
 			//If line != 9 --> wrongly entered and reinput line
 			if (input.length() != 9) {
@@ -44,13 +48,47 @@ public class SudokuSolver {
 	public void printBoard() {
 		for (int i = 0; i < 9; i++) {
 			for (int j = 0; j < 9; j++) {
-				System.out.print(boardArray[i][j]);
+				System.out.print(boardArray[i][j] + "|");
 			}
 			System.out.println();
 		}
 	}
 
-}
+	public boolean checkColumn() {
+		for (int i = 0; i < 9; i++) {
+			for (int j = 0; j < 9; j++) {
+				for (int k = j+1; k < 9; k++) {
+					if(boardArray[i][j] != 0 && boardArray[i][j] != boardArray[i][k]) {
+						return false;
+					}
+				}
+			}
+		}
 
+		return true;
+	}
+
+	public boolean checkRow() {
+		for (int i = 0; i < 9; i++) {
+			for (int j = 0; j < 9; j++) {
+				for (int k = j+1; j<9; j++) {
+					if (boardArray[j][i] !=0 && boardArray[j][i] != boardArray[j][k]) {
+						return false;
+					}
+				}
+			}
+		}
+
+		return true;
+	}
+
+	public boolean checkSquare() {
+		return false;
+	}
+	
+	public static void main(String[] args) {
+		SudokuSolver sudoku = new SudokuSolver();
+	}
+}
 
 

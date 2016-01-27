d3c2c2ce85aae4475bf6b00df6ee4d418f7b9811
kevintone <kevintone@hawaii.edu>
2016-01-26 22:40:22 -1000

    added functions to print board

--- SudokuSolver.java
+++ SudokuSolver.java
@@ -1,18 +1,29 @@
 /*
+ * SudokuSolver.java
+ * This sudoku solver will generate a solution to any valid sudoku puzzle
  *
- *
- *
+ * @author Kevin Tone
  */
 
 
 
 public class SudokuSolver() {
 	
+	private int boardArray[][];
+	
+		
 	public SudokuSolver()	{
+		//Initialize the board
+		boardArray = new int[9][9];
+	}
+	
+	public void getBoard() {
 
 	}
 
+	public void printBoard() {
 
+	}
 
 }
 
